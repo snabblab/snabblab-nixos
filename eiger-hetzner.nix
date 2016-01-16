d9c4a4878f38abc69afc25ed88b9a44b9f8639db
Domen Kožar <domen@dev.si>
2016-01-16 21:05:29 +0100

    eiger: also create raid1 on swap

--- eiger-hetzner.nix
+++ eiger-hetzner.nix
@@ -3,15 +3,15 @@
     deployment.targetEnv = "hetzner";
     deployment.hetzner.mainIPv4 = "136.243.111.220";
     deployment.hetzner.partitions = ''
-      clearpart --all --initlabel --drives=sda
+      clearpart --all --initlabel --drives=sda,sdb
 
-      part swap.1 --recommended --label=swap --fstype=swap --ondisk=sda
-      part swap.2 --recommended --label=swap --fstype=swap --ondisk=sdb
+      part raid.3 --size=8192 --ondisk=sda
+      part raid.4 --size=8192 --ondisk=sdb
+      part raid.1 --size=1 --grow --ondisk=sda
+      part raid.2 --size=1 --grow --ondisk=sdb
 
-      part raid.1 --size=6000 --ondisk=sda
-      part raid.2 --size=6000 --ondisk=sdb
-
-      raid / --level=1 --fstype=ext4 --label=root --device=md0 raid.1 raid.2
+      raid /    --level=1 --fstype=ext4 --device=md0 --label=root raid.1 raid.2
+      raid swap --level=1 --fstype=swap --device=md1 raid.3 raid.4
     '';
   };
 }
