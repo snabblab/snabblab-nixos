13c21bfb822d5c4b6bb007d485485f663f8d064d
Domen Kožar <domen@dev.si>
2016-01-16 19:52:20 +0100

    eiger: use raid1

--- eiger-hetzner.nix
+++ eiger-hetzner.nix
@@ -4,10 +4,14 @@
     deployment.hetzner.mainIPv4 = "136.243.111.220";
     deployment.hetzner.partitions = ''
       clearpart --all --initlabel --drives=sda
-      part swap --recommended --label=swap --fstype=swap --ondisk=sda
-      part / --fstype=ext4 --label=root --grow --ondisk=sda
+
+      part swap.1 --recommended --label=swap --fstype=swap --ondisk=sda
+      part swap.2 --recommended --label=swap --fstype=swap --ondisk=sdb
+
+      part raid.1 --size=6000 --ondisk=sda
+      part raid.2 --size=6000 --ondisk=sdb
+
+      raid / --level=1 --fstype=ext4 --label=root --device=md0 raid.1 raid.2
     '';
-    # if we don't set this, it tries to install bootloader on sdb
-    boot.loader.grub.devices = lib.mkForce ["/dev/sda"];
   };
 }
