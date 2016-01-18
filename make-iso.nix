965a7d20c5bd66c00b1a1e1144008927b11d34a4
Domen Kožar <domen@dev.si>
2016-01-18 10:32:19 +0100

    Nix expr for auto-installing ISO

---
+++ make-iso.nix
@@ -0,0 +1,79 @@
+# build an ISO image that will auto install NixOS and reboot
+# $ nix-build make-iso.nix
+
+
+# HOWTO: Use IPMIView to connect to machines using iKVM (to be able to boot this ISO)
+
+# (remotely) To launch an X framebuffer do
+# $ nix-shell -p xorg.xorgserver "Xvfb -screen 0 1024x768x16 -ac"
+
+# (remotely) Start IPMIView (patchelf patched for Nix)
+# $ DISPLAY=:0 ./IPMIView20
+
+# (locally) Setup VNC over SSH tunnel
+# $ ssh -L 5900:localhost:5900 eiger 'x11vnc -localhost -display :0 -ncache 10'
+
+# (locally) Fire up VNC session and login
+# $ vncviewer localhost
+
+
+let
+   config = (import <nixpkgs/nixos/lib/eval-config.nix> {
+     system = "x86_64-linux";
+     modules = [
+       <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+       ({ pkgs, lib, ... }:
+         let
+           cfg = pkgs.writeText "configuration.nix" ''
+            { config, pkgs, lib, ... }:
+
+            {
+              boot.loader.grub.enable = true;
+              boot.loader.grub.devices = [ "/dev/sda" ];
+              boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "ehci_pci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+
+              services.openssh.enable = true;
+              services.openssh.permitRootLogin = "yes";
+              users.extraUsers.root.initialHashedPassword = lib.mkForce "OhPha3gu";
+
+              # this is set for installed not to ask for password
+              users.mutableUsers = false;
+
+              fileSystems = [
+                { mountPoint = "/"; fsType = "ext4"; label = "root"; }
+              ];
+            }
+           '';
+           partitions = pkgs.writeText "partitions" ''
+             clearpart --all --initlabel --drives=sda
+             part swap --recommended --ondisk=sda
+             part / --fstype=ext4 --label=root --grow --ondisk=sda
+           '';
+         in {
+           systemd.services.inception = {
+             description = "Self-bootstrap a NixOS installation";
+             wantedBy = [ "multi-user.target" ];
+             after = [ "network.target" "polkit.service" ];
+             # TODO: submit a patch for blivet upstream to unhardcode kmod/e2fsprogs/utillinux
+             path = [ "/run/current-system/sw/" ];
+             script = with pkgs; ''
+               sleep 5
+               ${pythonPackages.nixpart0}/bin/nixpart ${partitions}
+               mkdir -p /mnt/etc/nixos/
+               cp ${cfg} /mnt/etc/nixos/configuration.nix
+               ${config.system.build.nixos-install}/bin/nixos-install -j 4
+               ${systemd}/bin/shutdown -r now
+             '';
+             environment = config.nix.envVars // {
+               inherit (config.environment.sessionVariables) NIX_PATH SSL_CERT_FILE;
+               HOME = "/root";
+             };
+             serviceConfig = {
+               Type = "oneshot";
+             };
+          };
+       })
+     ];
+   }).config;
+in
+  config.system.build.isoImage
