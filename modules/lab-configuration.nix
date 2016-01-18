e589b4aa7e3ed12187ef06be208990513c59a72f
Domen Kožar <domen@dev.si>
2016-01-18 18:35:15 +0100

    lab-configuration: blacklist i40e

--- modules/lab-configuration.nix
+++ modules/lab-configuration.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  require = [
+    ./common.nix
+  ];
   virtualisation.docker.enable = true;
   virtualisation.docker.storageDriver = "devicemapper";
   environment.systemPackages = with pkgs; [ docker ];
 
   services.openssh.enable = true;
+
+  # crashes with NICs
+  boot.blacklistedKernelModules = [ "i40e" ];
 }
