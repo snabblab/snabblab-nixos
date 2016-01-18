79d3d3a4f765c1723b7464868c07bf25dd352d3d
Domen Kožar <domen@dev.si>
2016-01-18 18:36:05 +0100

    common: allow ping, add some tools, add my pub key for root

--- modules/common.nix
+++ modules/common.nix
@@ -5,10 +5,16 @@
     ./users.nix
   ];
 
+  # less paranoia 
+  networking.firewall.allowPing = true;
+
   environment.systemPackages = with pkgs; [
     # develoment tools
-    gcc git gnumake wget
+    gcc git gnumake wget nmap screen tmux
     # editors
     emacs vim
   ];
+
+  # direct root access with pub key
+  users.extraUsers.root.openssh.authorizedKeys.keys = config.users.extraUsers.domenkozar.openssh.authorizedKeys.keys;
 }
