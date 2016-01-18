4a69041cf8f21cf0498c01feafb7b6215fdf4d33
Domen Kožar <domen@dev.si>
2016-01-18 18:34:11 +0100

    eiger: add samba

--- eiger.nix
+++ eiger.nix
@@ -6,6 +6,36 @@
       ./modules/common.nix
     ];
 
-    services.openssh.enable = true;
+    services.openssh = {
+      enable = true;
+    };
+
+    # samba is used for ISO booting for lab servers
+    users.extraUsers.smbguest = {
+      uid = 2000;
+      description = "smbguest";
+      group = "smbguest";
+    };
+    users.extraGroups.smbguest.gid = 2000;
+
+    networking.firewall.enable = true;
+    services.samba = {
+      enable = true;
+      shares = {
+        data =
+          { path = "/mnt/samba";
+            "read only" = "yes";
+            browseable = "yes";
+            "guest ok" = "yes";
+          };
+      };
+      extraConfig = ''
+        guest account = smbguest
+        map to guest = bad user
+      '';
+    };
+
+    # For IPMIView
+    environment.systemPackages = with pkgs; [ x11vnc ];
   };
 }
