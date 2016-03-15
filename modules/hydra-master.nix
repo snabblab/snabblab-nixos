01657669f421f290205eab7476dded03d2d5e80a
Domen Kožar <domen@dev.si>
2016-03-15 13:44:41 +0000

    nginx: remove commented line

--- modules/hydra-master.nix
+++ modules/hydra-master.nix
@@ -131,7 +131,6 @@ in {
 
         location / {
           proxy_pass http://127.0.0.1:8080;
-          #proxy_redirect http://127.0.0.1:8080/ http://$http_host/;
           proxy_set_header Host $http_host;
           proxy_set_header REMOTE_ADDR $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
