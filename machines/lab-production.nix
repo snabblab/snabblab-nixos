b7f39703b61f3ea1403f0832eb3b066c10fc6162
Lab <lab@snabb.co>
2016-05-16 15:27:35 +0000

    add 10 murren servers

--- machines/lab-production.nix
+++ machines/lab-production.nix
@@ -1,4 +1,20 @@
 let
+  mkHetzner = ip: { config, pkgs, lib, ... }: {
+    deployment.targetEnv = "hetzner";
+    deployment.hetzner.mainIPv4 = ip;
+    deployment.hetzner.partitions = ''
+      clearpart --all --initlabel --drives=sda,sdb
+
+      part raid.3 --size=8192 --ondisk=sda
+      part raid.4 --size=8192 --ondisk=sdb
+      part raid.1 --size=1 --grow --ondisk=sda
+      part raid.2 --size=1 --grow --ondisk=sdb
+
+      raid /    --level=1 --fstype=ext4 --device=md0 --label=root raid.1 raid.2
+      raid swap --level=1 --fstype=swap --device=md1 raid.3 raid.4
+    '';
+  };
+
   mkMachine = { ip, port ? 22, useGummiboot ? false }:
     { config, pkgs, lib, ... }: {
       deployment.targetEnv = "none";
@@ -25,4 +41,24 @@ in {
 
   grindelwald = mkMachine { ip = "lab1.snabb.co"; port = 2010; useGummiboot = true; };
   interlaken = mkMachine { ip = "lab1.snabb.co"; port = 2030; useGummiboot = true; };
+
+  # Hydra (CI) servers
+
+  murren-1 = mkHetzner "138.201.65.94";
+  murren-2 = mkHetzner "138.201.65.96";
+  murren-3 = mkHetzner "138.201.65.98";
+  murren-4 = mkHetzner "138.201.65.99";
+  murren-5 = mkHetzner "138.201.65.132";
+  murren-6 = mkHetzner "138.201.65.135";
+  murren-7 = mkHetzner "138.201.65.138";
+  murren-8 = mkHetzner "138.201.65.142";
+  murren-9 = mkHetzner "138.201.65.143";
+  murren-10 = mkHetzner "138.201.65.147";
+
+  #eiger  = mkHetzner "136.243.111.220";
+  #build-1 = mkHetzner "46.4.65.79";
+  #build-2 = mkHetzner "78.46.84.196";
+  #build-3 = mkHetzner "78.46.98.22";
+  #build-4 = mkHetzner "46.4.108.125";
+
 }
--- machines/lab.nix
+++ machines/lab.nix
@@ -1,40 +1,37 @@
-{
+let
+  defaults = {
+    require = [
+      ./../modules/lab-configuration.nix
+    ];
+  };
+in {
   network.description = "Snabb Lab machines";
 
-  lugano-1 = { config, pkgs, lib, ... }: {
+  lugano-1 = { config, pkgs, lib, ... }: defaults // {
+      # custom NixOS options here
+  };
+  lugano-2 = { config, pkgs, lib, ... }: defaults // {
       require = [
         ./../modules/lab-configuration.nix
       ];
       # custom NixOS options here
+      boot.kernelParams = lib.mkForce [ "intel_iommu=off" "hugepages=4096" "panic=60" ];
   };
-  lugano-2 = { config, pkgs, lib, ... }: {
-      require = [
-        ./../modules/lab-configuration.nix
-      ];
+  lugano-3 = { config, pkgs, lib, ... }: defaults // {
+      # custom NixOS options here
+      boot.kernelParams = lib.mkForce [ "intel_iommu=off" "hugepages=4096" "panic=60" ];
+      #boot.extraModprobeConfig = "options kvm-intel nested=y";
+      #boot.kernelModules = [ "pci-stub" ];
+      #boot.kernelParams = lib.mkForce [ "intel_iommu=on" "hugepages=4096" ];
+      #boot.blacklistedKernelModules = [ "ixgbe" ];
+  };
+  lugano-4 = { config, pkgs, lib, ... }: defaults // {
       # custom NixOS options here
   };
-  lugano-3 = { config, pkgs, lib, ... }: {
-      require = [
-        ./../modules/lab-configuration.nix
-      ];
+  grindelwald = { config, pkgs, lib, ... }: defaults // {
       # custom NixOS options here
   };
-  lugano-4 = { config, pkgs, lib, ... }: {
-      require = [
-        ./../modules/lab-configuration.nix
-      ];
-      # custom NixOS options here
-  };
-  grindelwald = { config, pkgs, lib, ... }: {
-      require = [
-        ./../modules/lab-configuration.nix
-      ];
-      # custom NixOS options here
-  };
-  interlaken = { config, pkgs, lib, ... }: {
-      require = [
-        ./../modules/lab-configuration.nix
-      ];
+  interlaken = { config, pkgs, lib, ... }: defaults // {
       fileSystems."/boot" = { 
         device = "/dev/disk/by-uuid/8AB0-B6D9";
         fsType = "vfat";
@@ -42,4 +39,18 @@
 
       # custom NixOS options here
   };
+
+  # Hydra (CI) servers
+
+  murren-1 = defaults;
+  murren-2 = defaults;
+  murren-3 = defaults;
+  murren-4 = defaults;
+  murren-5 = defaults;
+  murren-6 = defaults;
+  murren-7 = defaults;
+  murren-8 = defaults;
+  murren-9 = defaults;
+  murren-10 = defaults;
 }
+
