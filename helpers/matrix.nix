f8f6e268efdd3ec3dc875359170f853a3e1c9fd0
Domen Kožar <domen@dev.si>
2016-05-19 19:54:39 +0100

    matrix: build all dpdks

--- helpers/matrix.nix
+++ helpers/matrix.nix
@@ -65,12 +65,31 @@ let
        #  sha256 = "0hpnfdk96rrdaaf6qr4m4pgv40dw7r53mg95f22axj7nsyr8d72x";
        #})];
      });
+
+  buildDpdk = version: hash:
+     linuxPackages_4_1.dpdk.overrideDerivation (super: {
+       name = "dpdk-${version}";
+       inherit version;
+       prePatch = ''
+         find . -type f -exec sed -i 's/-Werror//' {} \;
+       '';
+       src = fetchurl {
+         url = "http://dpdk.org/browse/dpdk/snapshot/dpdk-${version}.tar.gz";
+         sha256 = hash;
+       };
+     });
   snabbs = [
     (buildSnabb "2016.03" "0wr54m0vr49l51pqj08z7xnm2i97x7183many1ra5bzzg5c5waky")
     (buildSnabb "2016.04" "1b5g477zy6cr5d9171xf8zrhhq6wxshg4cn78i5bki572q86kwlx")
     (buildSnabb "2016.05" "1xd926yplqqmgl196iq9lnzg3nnswhk1vkav4zhs4i1cav99ayh8")
   ];
   dpdks = [
+    (buildDpdk "16.04" "0yrz3nnhv65v2jzz726bjswkn8ffqc1sr699qypc9m78qrdljcfn")
+    (buildDpdk "2.2.0" "03b1pliyx5psy3mkys8j1mk6y2x818j6wmjrdvpr7v0q6vcnl83p")
+    (buildDpdk "2.1.0" "0h1lkalvcpn8drjldw50kipnf88ndv2wvflgkkyrmya5ga325czp")
+    (buildDpdk "2.0.0" "0gzzzgmnl1yzv9vs3bbdfgw61ckiakgqq93b9pc4v92vpsiqjdv4")
+    (buildDpdk "1.8.0" "0f8rvvp2y823ipnxszs9lh10iyiczkrhh172h98kb6fr1f1qclwz")
+    (buildDpdk "1.7.1" "0yd60ww5xhf0dfl2x1pqx1m2363b2b7zp89mcya86j20gi3bgvlx")
   ];
   qemus = [
     # TODO: https://hydra.snabb.co/build/4596
@@ -83,3 +102,4 @@ let
   ];
 in (listDrvToAttrs snabbs)
 // (listDrvToAttrs qemus)
+// (listDrvToAttrs dpdks)
