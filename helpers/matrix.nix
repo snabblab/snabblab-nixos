8e67de3e7cce08fac2523f6c9b575c60c7dbdbef
Domen Kožar <domen@dev.si>
2016-05-19 14:41:24 +0100

    add helpers/matrix.nix, very preliminary version for benchmarking a matrix of software

---
+++ helpers/matrix.nix
@@ -0,0 +1,65 @@
+ # Make a matrix out of Snabb + DPDK + QEMU + Linux (for iperf) 
+{}:
+
+with (import <nixpkgs> {});
+with (import ../lib.nix);
+with vmTools;
+
+let
+  # Snabb fixtures
+
+  # modules and NixOS config for plain qemu image
+  modules = [
+    <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ({config, pkgs, ...}: {
+      environment.systemPackages = with pkgs; [ screen python pciutils ethtool tcpdump netcat iperf ];
+      fileSystems."/".device = "/dev/disk/by-label/nixos";
+      boot.loader.grub.device = "/dev/sda";
+    })
+  ];
+  config = (import <nixpkgs/nixos/lib/eval-config.nix> { inherit modules; }).config;
+  # modules and NixOS config gor dpdk qmemu image
+  modules_dpdk = modules ++ [({config, pkgs, lib, ...}: {
+    # TODO
+  })];
+  config_dpdk = (import <nixpkgs/nixos/lib/eval-config.nix> { modules = modules_dpdk; }).config;
+  # files needed for some tests
+  test_env_nix = runCommand "test-env-nix" {} ''
+    mkdir -p $out
+    ln -s ${qemu_img}/nixos.qcow2 $out/qemu.img
+    ln -s ${qemu_dpdk_img}/nixos.qcow2 $out/qemu-dpdk.img
+    ln -s ${config.system.build.kernel}/bzImage $out/bzImage
+  '';
+  qemu_img = lib.makeOverridable (import <nixpkgs/nixos/lib/make-disk-image.nix>) {
+    inherit lib config pkgs;
+    partitioned = true;
+    format = "qcow2";
+    diskSize = 2 * 1024;
+  };
+  qemu_dpdk_img = qemu_img.override { config = config_dpdk; };
+
+  # build the matrix 
+
+  buildSnabb = version: hash:
+     snabbswitch.overrideDerivation (attrs: {
+       name = "snabb-${version}";
+       inherit version;
+       src = fetchFromGitHub {
+          owner = "snabbco";
+          repo = "snabb";
+          rev = "v${version}";
+          sha256 = hash;
+        };
+     });
+  snabbs = [
+    (buildSnabb "2016.03" "1baa477zy6cr5d9171xf8zrhhq6wxshg4cn78i5bki572q86kwlx")
+    (buildSnabb "2016.04" "1b5g477zy6cr5d9171xf8zrhhq6wxshg4cn78i5bki572q86kwlx")
+    (buildSnabb "2016.05" "1bag477zy6cr5d9171xf8zrhhq6wxshg4cn78i5bki572q86kwlx")
+  ];
+  dpdks = [
+  ];
+  qemus = [
+  ];
+  images = [
+  ];
+in listDrvToAttrs snabbs
