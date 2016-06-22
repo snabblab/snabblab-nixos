f22e28c963bc1ce258b8f3e3f55f2d9cf93c93d4
Domen Kožar <domen@dev.si>
2016-06-22 13:31:30 +0200

    simplify

--- machines/default.nix
+++ machines/default.nix
@@ -1,9 +1,11 @@
+with (import <nixpkgs> {});
+
 let
   evalMachine = name:
     let
       modules = [
-        (import ./../machines/eiger.nix).${name}
-        (import ./../machines/eiger-production.nix).${name}
+        (import ../machines/eiger.nix).${name}
+        (import ../machines/eiger-production.nix).${name}
         { networking.hostName = name; }
       ];
     in {
@@ -12,5 +14,5 @@ let
         imports = modules;
       };
     };
-in
-  (import <nixpkgs> {}).stdenv.lib.genAttrs ["build1" "build2" "build3" "build4"] evalMachine
+  machines = ["build1" "build2" "build3" "build4"];
+in stdenv.lib.genAttrs machines evalMachine
