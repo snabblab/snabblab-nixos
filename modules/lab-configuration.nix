21125c7ca9294ba2866268df7735edbc07048d6f
Domen Kožar <domen@dev.si>
2016-07-12 12:42:02 +0200

    intel_iommu=off on all lab servers

--- modules/lab-configuration.nix
+++ modules/lab-configuration.nix
@@ -64,9 +64,7 @@ with (import ../lib { inherit pkgs; });
   services.openssh.enable = true;
 
   # Disable IOMMU for Snabb Switch.
-  # chur has a Sandy Bridge CPU and these are known to have
-  # performance problems in their IOMMU.
-  boot.kernelParams = [ "intel_iommu=pt" "hugepages=4096" "panic=60"];
+  boot.kernelParams = [ "intel_iommu=off" "hugepages=4096" "panic=60"];
 
   # Used by snabb
   boot.kernelModules = [ "msr" ];
