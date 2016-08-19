ddbba111ff31a1c06ee4971332f5ba5639199635
Domen Kožar <domen@dev.si>
2016-08-19 18:20:08 +0200

    rename buildX to build-X to match hostnames

--- machines/default.nix
+++ machines/default.nix
@@ -1,12 +1,18 @@
 with (import <nixpkgs> {});
 
 let
+  mapping = {
+   build1 = "build-1";
+   build2 = "build-2";
+   build3 = "build-3";
+   build4 = "build-4";
+  };
   evalMachine = name:
     let
       modules = [
         (import ../machines/eiger.nix).${name}
         (import ../machines/eiger-production.nix).${name}
-        { networking.hostName = name; }
+        { networking.hostName = mapping.${name}; }
       ];
     in {
       eval = import <nixpkgs/nixos/lib/eval-config.nix> { inherit modules; };
