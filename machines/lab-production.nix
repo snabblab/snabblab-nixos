3c726dc867f7986377a04ad297d4d067ad700c79
Domen Kožar <domen@dev.si>
2016-09-08 12:36:35 +0200

    Add snabb2 machine

--- machines/lab-production.nix
+++ machines/lab-production.nix
@@ -42,6 +42,7 @@ in {
   grindelwald = mkMachine { ip = "lab1.snabb.co"; port = 2010; useGummiboot = true; };
   interlaken = mkMachine { ip = "lab1.snabb.co"; port = 2030; useGummiboot = true; };
   davos = mkMachine { ip = "lab1.snabb.co"; port = 2000; useGummiboot = false; };
+  snabb2 = mkMachine { ip = "igalia.com"; port = 4041; useGummiboot = false; };
 
   # Hydra (CI) servers
 
