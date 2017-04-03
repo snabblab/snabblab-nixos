snabblab/snabblab-nixos

commit 3006e32a53921803377541ef6cc293061b1102bf

Remove the with statements to help readability

lib/testing.nix
@@ -1,7 +1,5 @@
 { pkgs }:
 
-with pkgs;
-
 rec {
   # Function to build test_env qemu images needed for some benchmarks
   mkNixTestEnv = import ./test_env.nix { pkgs = pkgs; };
@@ -36,12 +34,12 @@ rec {
                 , alwaysSucceed ? false # if true, the build will succeed even on failure and provide a log
                 , ...
                 }@attrs:
-    stdenv.mkDerivation ((getPCIVars hardware) // {
+    pkgs.stdenv.mkDerivation ((getPCIVars hardware) // {
       src = snabb.src;
 
-      buildInputs = [ git telnet tmux numactl bc iproute which qemu utillinux ];
+      buildInputs = [ pkgs.git pkgs.telnet pkgs.tmux pkgs.numactl pkgs.bc pkgs.iproute pkgs.which pkgs.qemu pkgs.utillinux ];
 
-      SNABB_KERNEL_PARAMS = lib.optionalString needsNixTestEnv "init=/nix/var/nix/profiles/system/init";
+      SNABB_KERNEL_PARAMS = pkgs.lib.optionalString needsNixTestEnv "init=/nix/var/nix/profiles/system/init";
 
       postUnpack = ''
         patchShebangs .
@@ -60,7 +58,7 @@ rec {
         sed -i 's/testlog snabb/testlog/' src/Makefile
 
         mkdir -p $out/nix-support
-      '' + lib.optionalString needsNixTestEnv ''
+      '' + pkgs.lib.optionalString needsNixTestEnv ''
         mkdir ~/.test_env
         cp --no-preserve=mode -r ${testNixEnv}/* ~/.test_env/
       '';
@@ -69,10 +67,10 @@ rec {
 
       # http://unix.stackexchange.com/questions/14270/get-exit-status-of-process-thats-piped-to-another/73180#73180
       checkPhase =
-        lib.optionalString alwaysSucceed ''
+        pkgs.lib.optionalString alwaysSucceed ''
           set +o pipefail
         '' + checkPhase +
-        lib.optionalString alwaysSucceed ''
+        pkgs.lib.optionalString alwaysSucceed ''
           # if pipe failed, note that so it's eaiser to inspect end result
           [ "''${PIPESTATUS[0]}" -ne 0 ] && touch $out/nix-support/failed
           set -o pipefail
@@ -94,7 +92,7 @@ rec {
      } // removeAttrs attrs [ "checkPhase" ]);
 
   # Take a list of derivations and make an attribute set using their name attribute as key
-  listDrvToAttrs = list: builtins.listToAttrs (map (attrs: lib.nameValuePair (versionToAttribute attrs.name) attrs) list);
+  listDrvToAttrs = list: builtins.listToAttrs (map (attrs: pkgs.lib.nameValuePair (versionToAttribute attrs.name) attrs) list);
 
   /* Merge a list of attributesets. It is assumed keys that collide have the same value.
   
@@ -104,8 +102,8 @@ rec {
      => { a = "foo"; b = "bar"; }
 
   */
-  mergeAttrs = mergeAttrsMap lib.constant;
-  mergeAttrsMap = f: attrs: lib.foldl (x: y: x // (f y)) {} attrs;
+  mergeAttrs = mergeAttrsMap pkgs.lib.constant;
+  mergeAttrsMap = f: attrs: pkgs.lib.foldl (x: y: x // (f y)) {} attrs;
 
   /* Convert dots in the version to dashes.
      Reasoning: the version can be used as attribute key.

jobsets/snabb.nix
@@ -7,15 +7,12 @@
 , hardware ? "lugano"
 }:
 
-with pkgs;
-with lib;
-with vmTools;
-with (import ../lib { inherit pkgs; });
-
-rec {
+let
+  local_lib = import ../lib { };
+in rec {
   manual = import "${snabbSrc}/src/doc" {};
   snabb = import "${snabbSrc}" {};
-  tests = mkSnabbTest {
+  tests = local_lib.mkSnabbTest {
     name = "snabb-tests";
     inherit hardware snabb;
     needsNixTestEnv = true;
@@ -34,7 +31,7 @@ rec {
       cp src/qemu*.log $out/
     '';
   };
-  distro-builds = with diskImages; builtins.listToAttrs (map
+  distro-builds = with pkgs.vmTools.diskImages; builtins.listToAttrs (map
     (diskImage: {
        inherit (diskImage) name;
        value = runInLinuxImage (snabb // {

lib/benchmarks.nix
@@ -4,11 +4,10 @@
 # collecting results by parsing logs and converting them to CSV and
 # generating reports using Rmarkdown.
 
-with pkgs;
-with (import ./testing.nix { inherit pkgs; });
-with (import ./software.nix { inherit pkgs; });
-
-rec {
+let
+  testing = import ./testing.nix { };
+  software = import ./software.nix { inherit pkgs; };
+in rec {
   /* Execute a benchmark named as specified using `name` parameter,
      repeated as many times as the integer `times`.
 
@@ -25,7 +24,7 @@ rec {
   mkSnabbBenchTest = { name, times, toCSV, ... }@attrs:
    let
      # patch needed for Snabb v2016.05 and lower
-     testEnvPatch = fetchurl {
+     testEnvPatch = software.fetchurl {
        url = "https://github.com/snabbco/snabb/commit/e78b8b2d567dc54cad5f2eb2bbb9aadc0e34b4c3.patch";
        sha256 = "1nwkj5n5hm2gg14dfmnn538jnkps10hlldav3bwrgqvf5i63srwl";
      };
@@ -33,7 +32,7 @@ rec {
        let
          name' = "${name}_num=${toString num}";
        in {
-         ${name'} = lib.hydraJob (mkSnabbTest ({
+         ${name'} = pkgs.lib.hydraJob (software.mkSnabbTest ({
            name = name';
            alwaysSucceed = true;
            patchPhase = ''
@@ -53,7 +52,7 @@ rec {
            } // (attrs.meta or {});
          } // removeAttrs attrs [ "times" "toCSV" "dpdk" "kPackages" "meta" "name"]));
        };
-   in mergeAttrsMap snabbBenchmark (lib.range 1 times);
+   in software.mergeAttrsMap snabbBenchmark (pkgs.lib.range 1 times);
 
   /* Execute `basic1` benchmark.
 
@@ -62,7 +61,7 @@ rec {
   */
   mkMatrixBenchBasic = { snabb, times, hardware ? "murren", ... }:
     mkSnabbBenchTest {
-      name = "basic1_snabb=${versionToAttribute snabb.version or ""}_packets=100e6";
+      name = "basic1_snabb=${software.versionToAttribute snabb.version or ""}_packets=100e6";
       inherit snabb times hardware;
       checkPhase = ''
         /var/setuid-wrappers/sudo ${snabb}/bin/snabb snabbmark basic1 100e6 |& tee $out/log.txt
@@ -80,7 +79,7 @@ rec {
   */
   mkMatrixBenchPacketblaster = { snabb, times, hardware ? "lugano", ... }:
     mkSnabbBenchTest {
-      name = "${versionToAttribute snabb.version or ""}-packetblaster-64";
+      name = "${software.versionToAttribute snabb.version or ""}-packetblaster-64";
       inherit snabb times hardware;
       toCSV = drv: ''
         pps=$(cat ${drv}/log.txt | grep TXDGPC | cut -f 3 | sed s/,//g)
@@ -101,7 +100,7 @@ rec {
   */
   mkMatrixBenchPacketblasterSynth = { snabb, times, ... }:
     mkSnabbBenchTest {
-      name = "${versionToAttribute snabb.version or ""}-packetblaster-synth-64";
+      name = "${software.versionToAttribute snabb.version or ""}-packetblaster-synth-64";
       inherit snabb times;
       hardware = "lugano";
       toCSV = drv: ''
@@ -133,9 +132,9 @@ rec {
         l2tpv3_ipsec = "program/snabbnfv/test_fixtures/nfvconfig/test_functions/crypto-tunnel.ports";
       };
     in mkSnabbBenchTest {
-      name = "iperf_conf=${conf}_snabb=${versionToAttribute snabb.version or ""}_kernel=${versionToAttribute kPackages.kernel.version}_qemu=${versionToAttribute qemu.version}";
+      name = "iperf_conf=${conf}_snabb=${software.versionToAttribute snabb.version or ""}_kernel=${software.versionToAttribute kPackages.kernel.version}_qemu=${software.versionToAttribute qemu.version}";
       inherit hardware kPackages snabb times qemu;
-      testNixEnv = mkNixTestEnv { inherit kPackages; };
+      testNixEnv = software.mkNixTestEnv { inherit kPackages; };
       toCSV = drv: ''
         score=$(awk '/^IPERF-/ { print $2 }' < ${drv}/log.txt)
         ${writeCSV drv "iperf" "Gbps"}
@@ -165,13 +164,13 @@ rec {
     in
     # there is no reason to run this benchmark on multiple kernels
     # 3.18 kernel must be used for older dpdks
-    if (lib.substring 0 4 (kPackages.kernel.version) != "3.18")
+    if (pkgs.lib.substring 0 4 (kPackages.kernel.version) != "3.18")
     then []
     else mkSnabbBenchTest rec {
-      name = "l2fwd_pktsize=${pktsize}_conf=${conf}_snabb=${versionToAttribute snabb.version or ""}_dpdk=${versionToAttribute dpdk.version}_qemu=${versionToAttribute qemu.version}";
+      name = "l2fwd_pktsize=${pktsize}_conf=${conf}_snabb=${software.versionToAttribute snabb.version or ""}_dpdk=${software.versionToAttribute dpdk.version}_qemu=${software.versionToAttribute qemu.version}";
       inherit snabb qemu times hardware dpdk kPackages;
       needsNixTestEnv = true;
-      testNixEnv = mkNixTestEnv { inherit kPackages dpdk; };
+      testNixEnv = software.mkNixTestEnv { inherit kPackages dpdk; };
       toCSV = drv: ''
         score=$(awk '/^Rate\(Mpps\):/ { print $2 }' < ${drv}/log.txt)
         ${writeCSV drv "l2fwd" "Mpps"}
@@ -201,18 +200,18 @@ rec {
 
   # Generate CSV out of collection of benchmarking logs
   mkBenchmarkCSV = benchmarkList:
-   stdenv.mkDerivation {
+   pkgs.stdenv.mkDerivation {
      name = "snabb-report-csv";
      buildInputs = [ pkgs.gawk pkgs.bc ];
      # Build CSV on Hydra localhost to spare time on copying
      requiredSystemFeatures = [ "local" ];
      # TODO: uses writeText until following is merged https://github.com/NixOS/nixpkgs/pull/15803
-     builder = writeText "csv-builder.sh" ''
+     builder = software.writeText "csv-builder.sh" ''
        source $stdenv/setup
        mkdir -p $out/nix-support
 
        echo "benchmark,pktsize,config,snabb,kernel,qemu,dpdk,id,score,unit" > $out/bench.csv
-       ${lib.concatMapStringsSep "\n" (drv: drv.meta.toCSV drv) benchmarkList}
+       ${pkgs.lib.concatMapStringsSep "\n" (drv: drv.meta.toCSV drv) benchmarkList}
 
        # Make CSV file available via Hydra
        echo "file CSV $out/bench.csv" >> $out/nix-support/hydra-build-products
@@ -223,18 +222,18 @@ rec {
       generate a report using Rmarkdown.
    */
    mkBenchmarkReport = csv: benchmarksList: reportName:
-    stdenv.mkDerivation {
+    software.stdenv.mkDerivation {
       name = "snabb-report";
-      buildInputs = [ rPackages.rmarkdown rPackages.ggplot2 rPackages.dplyr R pandoc which ];
+      buildInputs = [ software.rPackages.rmarkdown software.rPackages.ggplot2 software.rPackages.dplyr software.R software.pandoc software.which ];
       # Build reports on Hydra localhost to spare time on copying
       requiredSystemFeatures = [ "local" ];
       # TODO: use writeText until runCommand uses passAsFile (16.09)
-      builder = writeText "csv-builder.sh" ''
+      builder = software.writeText "csv-builder.sh" ''
         source $stdenv/setup
 
         # Store all logs
         mkdir -p $out/nix-support
-        ${lib.concatMapStringsSep "\n" (drv: "cat ${drv}/log.txt > $out/${drv.name}-${toString drv.meta.repeatNum}.log") benchmarksList}
+        ${pkgs.lib.concatMapStringsSep "\n" (drv: "cat ${drv}/log.txt > $out/${drv.name}-${toString drv.meta.repeatNum}.log") benchmarksList}
         tar cfJ logs.tar.xz -C $out .
         mv logs.tar.xz $out/
         echo "file tarball $out/logs.tar.xz" >> $out/nix-support/hydra-build-products
@@ -253,30 +252,30 @@ rec {
 
    # Generate a list of names of available reports in `./lib/reports`
    listReports =
-     map (name: lib.removeSuffix ".Rmd" name)
+     map (name: pkgs.lib.removeSuffix ".Rmd" name)
          (builtins.attrNames (builtins.readDir ../lib/reports));
 
    # Returns true if version is a prefix of drv.version
    matchesVersionPrefix = version: drv:
-     lib.hasPrefix version (lib.getVersion drv);
+     pkgs.lib.hasPrefix version (pkgs.lib.getVersion drv);
 
    # Select software collections based on version strings
    selectQemus = versions:
      if versions == []
-     then qemus
-     else lib.concatMap (version: lib.filter (matchesVersionPrefix version) qemus) versions;
+     then software.qemus
+     else pkgs.lib.concatMap (version: pkgs.lib.filter (matchesVersionPrefix version) software.qemus) versions;
    selectDpdks = versions: kPackages:
      if versions == []
-     then (dpdks kPackages)
-     else lib.concatMap (version: lib.filter (matchesVersionPrefix version) (dpdks kPackages)) versions;
+     then (software.dpdks kPackages)
+     else pkgs.lib.concatMap (version: pkgs.lib.filter (matchesVersionPrefix version) (software.dpdks kPackages)) versions;
    selectKernelPackages = versions:
      if versions == []
-     then kernelPackages
-     else lib.concatMap (version: lib.filter (kPackages: lib.hasPrefix version (lib.getVersion kPackages.kernel)) kernelPackages) versions;
+     then software.kernelPackages
+     else pkgs.lib.concatMap (version: pkgs.lib.filter (kPackages: pkgs.lib.hasPrefix version (pkgs.lib.getVersion kPackages.kernel)) software.kernelPackages) versions;
 
    # Given a list of names and benchmark inputs/parameters, get benchmarks by their alias and pass them the parameters
    selectBenchmarks = names: params:
-     mergeAttrsMap (name: (lib.getAttr name benchmarks) params) names;
+     software.mergeAttrsMap (name: (pkgs.lib.getAttr name benchmarks) params) names;
 
    # Benchmarks aliases that can be referenced using just a name, i.e. "iperf-filter"
    benchmarks = {

lib/default.nix
@@ -1,7 +1,5 @@
 { pkgs ? import <nixpkgs> {}}:
 
-with pkgs;
-
 let
   benchmarks = import ./benchmarks.nix { pkgs = pkgs; };
   testing = import ./testing.nix { pkgs = pkgs; };

lib/software.nix
@@ -2,15 +2,13 @@
 
 # build* functions are responsible for building software given their source or version
 
-with pkgs;
-
 rec {
   # Build Snabb using git version tag
   buildSnabb = version: hash:
-     snabbswitch.overrideDerivation (super: {
+     pkgs.snabbswitch.overrideDerivation (super: {
        name = "snabb-${version}";
        inherit version;
-       src = fetchFromGitHub {
+       src = pkgs.fetchFromGitHub {
           owner = "snabbco";
           repo = "snabb";
           rev = "v${version}";
@@ -23,7 +21,7 @@ rec {
    if snabbSrc == null
    then null
    else
-     (callPackage snabbSrc {}).overrideDerivation (super:
+     (pkgs.callPackage snabbSrc {}).overrideDerivation (super:
        {
          name = super.name + version;
          inherit version;
@@ -32,7 +30,7 @@ rec {
 
   buildQemu = version: hash: applySnabbPatch:
     let
-       src = fetchurl {
+       src = pkgs.fetchurl {
          url = "http://wiki.qemu.org/download/qemu-${version}.tar.bz2";
          sha256 = hash;
        };
@@ -45,23 +43,23 @@ rec {
          sha256 = "0hpnfdk96rrdaaf6qr4m4pgv40dw7r53mg95f22axj7nsyr8d72x";
          name = "snabb-patch";
        };
-     in qemu.overrideDerivation (super: {
-       name = "qemu-${version}" + lib.optionalString applySnabbPatch "-with-snabbpatch";
-       version = version + lib.optionalString applySnabbPatch "-with-snabbpatch";
+     in pkgs.qemu.overrideDerivation (super: {
+       name = "qemu-${version}" + pkgs.lib.optionalString applySnabbPatch "-with-snabbpatch";
+       version = version + pkgs.lib.optionalString applySnabbPatch "-with-snabbpatch";
        inherit src;
        patchPhase = ''
          substituteInPlace Makefile --replace \
            "install-datadir install-localstatedir" \
            "install-datadir" \
            --replace "install-sysconfig " ""
-       '' + lib.optionalString applySnabbPatch ''
+       '' + pkgs.lib.optionalString applySnabbPatch ''
          patch -p1 < ${snabbPatch}
        '';
      });
 
   buildDpdk = version: hash: kPackages:
     let
-      src = fetchurl {
+      src = pkgs.fetchurl {
         url = "http://dpdk.org/browse/dpdk/snapshot/dpdk-${version}.tar.gz";
         sha256 = hash;
       };
@@ -69,10 +67,10 @@ rec {
 
   buildDpdkFromSrc = version: src: kPackages:
     let
-      origDpdk = callPackage ../pkgs/dpdk.nix { kernel = kPackages.kernel; };
-      needsGCC49 = lib.any (v: v == version) ["1.7.1" "1.8.0" "2.0.0" "2.1.0"];
+      origDpdk = pkgs.callPackage ../pkgs/dpdk.nix { kernel = kPackages.kernel; };
+      needsGCC49 = pkgs.lib.any (v: v == version) ["1.7.1" "1.8.0" "2.0.0" "2.1.0"];
       dpdk = if needsGCC49
-             then (origDpdk.override { stdenv = overrideCC stdenv gcc48;})
+             then (origDpdk.override { stdenv = pkgs.overrideCC pkgs.stdenv pkgs.gcc48;})
              else origDpdk;
     in dpdk.overrideDerivation (super: {
       name = "dpdk-${version}-${kPackages.kernel.version}";
@@ -108,10 +106,10 @@ rec {
   ];
 
   kernelPackages = [
-    linuxPackages_3_14
-    linuxPackages_3_18
-    linuxPackages_4_1
-    linuxPackages_4_3
-    linuxPackages_4_4
+    pkgs.linuxPackages_3_14
+    pkgs.linuxPackages_3_18
+    pkgs.linuxPackages_4_1
+    pkgs.linuxPackages_4_3
+    pkgs.linuxPackages_4_4
   ];
 }

lib/test_env.nix
@@ -5,8 +5,6 @@
 
 { pkgs }:
 
-with pkgs;
-
 { kPackages ? linuxPackages
 , dpdk ? linuxPackages.dpdk }:
    let

commit 8621ed424fe5cbdb1c833f76efb763b53231188b

testing: use the specified qemu instead of default
This was broken since 3006e32a in September 2016

lib/testing.nix
@@ -37,7 +37,7 @@ rec {
     pkgs.stdenv.mkDerivation ((getPCIVars hardware) // {
       src = snabb.src;
 
-      buildInputs = [ pkgs.git pkgs.telnet pkgs.tmux pkgs.numactl pkgs.bc pkgs.iproute pkgs.which pkgs.qemu pkgs.utillinux ];
+      buildInputs = [ pkgs.git pkgs.telnet pkgs.tmux pkgs.numactl pkgs.bc pkgs.iproute pkgs.which qemu pkgs.utillinux ];
 
       SNABB_KERNEL_PARAMS = pkgs.lib.optionalString needsNixTestEnv "init=/nix/var/nix/profiles/system/init";
 