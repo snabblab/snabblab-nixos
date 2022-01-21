113eaff914b5817ec4e8818abc36a0af4e86e9a0
Max Rottenkolber <max@mr.gy>
2022-01-21 13:36:45 +0000

    lib/testing.nix: add python3 dependency (lwaftr tests)

--- lib/testing.nix
+++ lib/testing.nix
@@ -37,7 +37,7 @@ rec {
     pkgs.stdenv.mkDerivation ((getPCIVars hardware) // {
       src = snabb.src;
 
-      buildInputs = [ pkgs.git pkgs.telnet pkgs.tmux pkgs.numactl pkgs.bc pkgs.iproute pkgs.which qemu pkgs.utillinux ];
+      buildInputs = [ pkgs.git pkgs.telnet pkgs.tmux pkgs.numactl pkgs.bc pkgs.iproute pkgs.which qemu pkgs.utillinux pkgs.python3 ];
 
       SNABB_KERNEL_PARAMS = pkgs.lib.optionalString needsNixTestEnv "init=/nix/var/nix/profiles/system/init";
 
