944230ac3e98dccf529f6390bbdfb04c78b29a4f
Max Rottenkolber <max@mr.gy>
2022-04-07 13:40:12 +0200

    testing.nix: add CPU groups for nfg2 hw class

--- lib/testing.nix
+++ lib/testing.nix
@@ -13,6 +13,8 @@ rec {
     };
     nfg2 = {
       SNABB_CPUS = "6-23";
+      SNABB_CPUS0 = "6-11";
+      SNABB_CPUS1 = "12-17";
       SNABB_PCI_CONNECTX_0 = "0000:81:00.0";
       SNABB_PCI_CONNECTX_1 = "0000:81:00.1";
     };
